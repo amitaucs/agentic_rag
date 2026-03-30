74fd8b701f0ec30fc719fbaf40e863846672f582
Amit Datta <amitdatta@Amits-MacBook-Pro-2.local>
2026-03-30 06:37:37 +0530

    updated

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+
+from graph.chains.hallucination_grader import GradeHallucinations, hallucination_grader
+
 load_dotenv()
 
 from graph.chains.retrieval_grader import retrieval_grader,GradeDocument
@@ -35,3 +38,25 @@ def test_generation_chain() -> None:
     generation = generation_chain.invoke({"context": docs, "question": question})
     print(generation)
 
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
